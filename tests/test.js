64a8d455bbcde1587120706eb15b607d9a9daee4
JJ Marquis <76982850+JJMrqs@users.noreply.github.com>
2021-07-22 17:17:37 -0600

    fixes tests for testing purposes

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 describe('This is my first test', () => {
   test('and it does this', () => {
-    expect(false).toBe(true);
+    expect(true).toBe(true);
   });
 
   test('and this second test does this', () => {

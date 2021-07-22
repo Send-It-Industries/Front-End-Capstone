6e8b29781a5d3e31bf801a09dbb4997012915ef5
Keanu Hasty <keanu.hasty@gmail.com>
2021-07-22 16:29:14 -0600

    added test directory and sample test file

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+describe('This is my first test', () => {
+  test('and it does this', () => {
+    expect(true).toBe(true);
+  });
+
+  test('and this second test does this', () => {
+    expect(false).toBe(false);
+  });
+});

6b94754e2798160c1f6f684b1c4d03d4c17d5f41
Char-les-G <charlesgivens80@gmail.com>
2021-07-26 13:03:51 -0700

    added files for product overview

---
+++ client/Components/Product Information/Styles.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// this file will contain thumbnails detailing the style of the selected product
+  // display will be as many as are available, but no more than 4 per row

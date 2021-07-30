59ea5695c402ccd0cf7f65c3118922f08faab4c5
Keanu Hasty <keanu.hasty@gmail.com>
2021-07-29 19:04:53 -0600

    Style selector changes current style and highlights selected style

--- client/Components/ProductOverview/StyleSelector.jsx
+++ client/Components/ProductOverview/StyleSelector.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../Contexts/AppContext';
 
 const StyleSelector = () => {
   const name = 'Style Selector';
+  const {
+    productInfo,
+    selectedStyle,
+    displayImageIndex,
+    setDisplayImageIndex,
+    setSelectedStyle,
+  } = useContext(AppContext);
+  const selected = { border: '5px solid blue' };
+
+  const handleStyleChange = (newStyle, newIndex) => {
+    setSelectedStyle(newStyle);
+    // setDisplayImageIndex(newIndex);
+  };
+  // setDisplayImageIndex(newIndex)
+
   return (
     <div>
       <h5>{name}</h5>
-      <div>Style Select thumbnails go here</div>
+      {productInfo.styles.map((style, i) => (
+        <img
+          style={(style.style_id === selectedStyle.style_id) ? selected : null}
+          src={style.photos[displayImageIndex].thumbnail_url}
+          alt={`Style Thumbnail for style ID: ${style.style_id}`}
+          key={i}
+          onClick={() => (handleStyleChange(style, i))} />
+      ))}
     </div>
   );
 };

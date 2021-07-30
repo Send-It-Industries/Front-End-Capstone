3c5cca6d7190a315df898706781d5103a2147c33
Richard Cuffee <rgcuffee@gmail.com>
2021-07-30 15:54:43 -0700

    removed lint

--- client/Components/Helpers/Helpful.jsx
+++ client/Components/Helpers/Helpful.jsx
@@ -1,25 +1,22 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable arrow-body-style */
 import React from 'react';
 
-const Helpful = ({ value }) => {
-  return (
-    <div>
-      Helpful?
-      {' '}
-      <span
-        onClick={() => {
-          console.log('You are like so helpful rn');
-        }}
-        style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
-      >
-        Yes
-      </span>
-      (
-      { value }
-      )
-    </div>
-  );
-};
+const Helpful = ({ value }) => (
+  <div>
+    Helpful?
+    {' '}
+    <span
+      onClick={() => {
+        console.log('You are like so helpful rn');
+      }}
+      style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
+    >
+      Yes
+    </span>
+    (
+    {value}
+    )
+  </div>
+);
 
 export default Helpful;
--- client/Components/Helpers/Report.jsx
+++ client/Components/Helpers/Report.jsx
@@ -1,20 +1,17 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable arrow-body-style */
 import React from 'react';
 
-const Report = () => {
-  return (
-    <div>
-      <span
-        onClick={() => {
-          console.log('Im tElLiNgG!');
-        }}
-        style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
-      >
-        Report
-      </span>
-    </div>
-  );
-};
+const Report = () => (
+  <div>
+    <span
+      onClick={() => {
+        console.log('Im tElLiNgG!');
+      }}
+      style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
+    >
+      Report
+    </span>
+  </div>
+);
 
 export default Report;

8ee6adce0fb9d0a859c7520ad6b4b760bea7f1c9
Keanu Hasty <keanu.hasty@gmail.com>
2021-07-30 17:38:19 -0600

    size and quantity select working

--- client/Components/ProductOverview/SKUSelector.jsx
+++ client/Components/ProductOverview/SKUSelector.jsx
@@ -1,15 +1,115 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { range } from 'underscore';
+
+import AppContext from '../Contexts/AppContext';
 
 const SKUSelector = () => {
   const name = 'Sku Selector';
+  const { selectedStyle } = useContext(AppContext);
+
+  const [inStock, setInStock] = useState(true);
+  const [selectedSku, setSelectedSku] = useState('sizeSelect');
+  const [sizeSelected, setSizeSelected] = useState(false);
+  const [wantedQuantity, setWantedQuantity] = useState('-');
+  const [maxQuantity, setMaxQuantity] = useState(15);
+
+  const handleSizeSelect = (e) => {
+    let maxQ;
+    let wantedQ;
+    setSelectedSku(e.target.value);
+    setSizeSelected(true);
+    if (selectedStyle.skus[e.target.value].quantity < 15) {
+      maxQ = selectedStyle.skus[e.target.value].quantity;
+    } else {
+      maxQ = 15;
+    }
+    setMaxQuantity(maxQ);
+    if (wantedQuantity === '-') {
+      setWantedQuantity(1);
+    } else if (wantedQuantity > maxQ) {
+      setWantedQuantity(maxQ);
+    }
+  };
+
+  const handleQuantitySelect = (e) => {
+    debugger;
+    setWantedQuantity(e.target.value);
+  };
+
+  const selectedStyleInStock = () => {
+    // Two ways an item can be out of stock:
+    //   1. skus object contains 1 key value pair containing null instead of a sku
+    //                                           { null: { quantity: null,
+    //                                                     size: null } }
+    //   2. skus abject contains key values pairs with sizes and zero values
+    //                                           { <sku#>: { quantity: 0,
+    //                                                       size: "One Size" } }
+    // Check the current style has skus available by checking if the skus object has keys
+    // then if the skus are available sum the number of available items per style.
+    // convert truthy/falsy to boolean with !!
+    const skusAvailable = !!(Object.keys(selectedStyle.skus).length);
+    return (skusAvailable ? !!(
+      Object.values(selectedStyle.skus).reduce((sum, { quantity }) => (
+        sum + quantity
+      ), 0)
+    ) : false);
+  };
+
+  useEffect(() => {
+    if (selectedStyleInStock()) {
+      setInStock(true);
+      setSelectedSku('sizeSelect');
+    } else {
+      setInStock(false);
+      setSelectedSku('outOfStock');
+    }
+    setSizeSelected(false);
+    setWantedQuantity('-');
+  }, [selectedStyle]);
+
   return (
     <div>
       <h5>{name}</h5>
-      <select>
-        <option>size</option>
+      {/* Size Select */}
+      <select
+        value={selectedSku}
+        id="sizes"
+        onChange={(e) => (handleSizeSelect(e, selectedSku))}
+        disabled={!inStock}
+      >
+        {
+          inStock ? (
+            <>
+              <option disabled value="sizeSelect">Select Size</option>
+              {
+              Object.entries(selectedStyle.skus)
+                .filter(([, { quantity }]) => (quantity))
+                .map(([sku, { size }]) => (
+                  <option value={sku} key={sku}>{size}</option>
+                ))
+              }
+            </>
+          ) : (
+            <option value="outOfStock">Out Of Stock</option>
+          )
+        }
       </select>
-      <select>
-        <option>Quantity</option>
+      {/* Quantity select */}
+      <select
+        value={wantedQuantity}
+        id="quantity"
+        disabled={!sizeSelected}
+        onChange={handleQuantitySelect}
+      >
+        {
+          sizeSelected ? (
+            range(1, maxQuantity + 1).map((possibleQuantity) => (
+              <option value={possibleQuantity} key={possibleQuantity}>{possibleQuantity}</option>
+            ))
+          ) : (
+            <option value>{wantedQuantity}</option>
+          )
+        }
       </select>
       <button type="button">Add to Bag</button>
       <button type="button">Social Media? outfit?</button>

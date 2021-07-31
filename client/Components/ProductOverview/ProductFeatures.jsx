7e8761c546e55aa7e2e098f68523657e9ecbed9b
Keanu Hasty <keanu.hasty@gmail.com>
2021-07-31 12:14:25 -0600

    change default product

--- client/Components/ProductOverview/ProductFeatures.jsx
+++ client/Components/ProductOverview/ProductFeatures.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AppContext from '../Contexts/AppContext';
 
 const ProductFeatures = () => {
   const name = 'Product Features';
+  const { productInfo } = useContext(AppContext);
   return (
     <div>
       <h4>{name}</h4>
+      <div>{productInfo.slogan}</div>
+      <div>{productInfo.description}</div>
+      {
+        productInfo.features.map(({ feature, value }) => (
+          <div>
+            {`${feature}${value ? ` - ${value}` : ''}`}
+          </div>
+        ))
+      }
     </div>
   );
 };

7400b410b8bca00da70d46febc46e4ae16d19321
rgcuffee <rgcuffee@gmail.com>
2021-08-02 09:15:25 -0700

    refactored Questions.jsx

--- client/Components/QA/Questions.jsx
+++ client/Components/QA/Questions.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable linebreak-style */
-import React, { useEffect, useState } from "react";
-import AddAnswer from "./AddAnswer";
-import Answers from "./Answers";
-import Helpful from "../Helpers/Helpful";
+import React, { useEffect, useState } from 'react';
+import AddAnswer from './AddAnswer';
+import Answers from './Answers';
+import Helpful from '../Helpers/Helpful';
 
 const Questions = ({ Q }) => {
-  const [answers, setAnswers] = useState("");
+  const [answers, setAnswers] = useState('');
   const [answerCount, setAnswerCount] = useState(2);
   const [totalAnswers, setTotalAnswers] = useState(2);
 
@@ -17,26 +19,26 @@ const Questions = ({ Q }) => {
     setAnswers(Q.answers);
     setAnswerCount(answerCount);
     setTotalAnswers(Object.values(answers).length);
-    console.log(`count:${answerCount}   total:${totalAnswers}`);
+    // console.log(`count:${answerCount}   total:${totalAnswers}`);
   });
 
-  console.log('length: ' + Object.values(answers).length);
+  // console.log(Q.answers);
 
   return (
     <div>
       <div
         style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          border: "1px solid black",
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          border: '1px solid black',
         }}
       >
-        <span style={{ fontWeight: "bold" }}>
+        <span style={{ fontWeight: 'bold' }}>
           Q:
           {Q.question_body}
         </span>
-        <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
           <Helpful value={Q.question_helpfulness} />
           |
           <AddAnswer question_id={Q.question_id} />
@@ -44,7 +46,7 @@ const Questions = ({ Q }) => {
       </div>
       <div
         style={{
-          border: "1px solid black",
+          border: '1px solid black',
         }}
       >
         {Object.entries(Q.answers)
@@ -52,25 +54,31 @@ const Questions = ({ Q }) => {
           .map((answer) => (
             <Answers answer={answer[1]} key={answer[0]} />
           ))}
-        <span
-          onClick={moreAnswers}
-          style={{ visibility: answerCount >= totalAnswers ? 'hidden' : 'visible', fontWeight: "bold", cursor: "pointer" }}
-        >
-          Load More Answers
-        </span>
-        {/* Add Collapse Answers Btn here */}
-        <span
-          style={{
-            visibility: answerCount > 2 ? "visible" : "hidden",
-            fontWeight: "bold",
-            cursor: "pointer",
-          }}
-          onClick={() => {
-            setAnswerCount(2);
-          }}
-        >
-          Collapse Answers
-        </span>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span
+            onClick={moreAnswers}
+            style={{
+              visibility: answerCount >= totalAnswers ? 'hidden' : 'visible',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+            }}
+          >
+            Load More Answers
+          </span>
+          {/* Add Collapse Answers Btn here */}
+          <span
+            style={{
+              visibility: (answerCount >= totalAnswers && answerCount > 2) ? 'visible' : 'hidden',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              setAnswerCount(2);
+            }}
+          >
+            Collapse Answers
+          </span>
+        </div>
       </div>
     </div>
   );

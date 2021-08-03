0d8dc30892ee98649b995ba8753eb76085734eb4
rgcuffee <rgcuffee@gmail.com>
2021-08-03 10:39:29 -0700

    full implementation refactor of helpful/reoprt.jsx

--- client/Components/Helpers/Helpful.jsx
+++ client/Components/Helpers/Helpful.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable linebreak-style */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 // takes in review/report/question...?
@@ -15,31 +15,19 @@ const Helpful = ({ value, component, componentId }) => {
   const [helpfulCount, setHelpfulCount] = useState(value);
   const [clicked, setClicked] = useState(false);
 
-  useEffect(() => {
-    setHelpfulCount(helpfulCount);
-  }, [helpfulCount]);
-
-  const incrementHelpfulCount = () => {
-    setHelpfulCount(helpfulCount + 1);
-    setClicked(true);
-  };
-
   const handleUpdate = (component, componentId) => {
     if (component === 'qa') {
       axios.put(`/api/qa/questions/${componentId}/helpful`).then(() => {
-        console.log('updated');
       });
     } else if (component === 'answers') {
       axios.put(`/api/qa/answers/${componentId}/helpful`).then(() => {
-        console.log('updated');
       });
     } else if (component === 'reviews') {
       axios.put(`/api/reviews/${componentId}/helpful`).then(() => {
-        console.log('updated');
       });
-      // console.log(component)
     }
-    incrementHelpfulCount();
+    setHelpfulCount(helpfulCount + 1);
+    setClicked(true);
   };
   return (
     <div>

7f1e1caf1b235e7b229dfca4cf95bd515f5f0b72
JJ Marquis <76982850+JJMrqs@users.noreply.github.com>
2021-08-05 15:45:15 -0600

    partial setup of add reviews modal

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   // ------------------------------------------------------------------------------------
   // ------------------                State                 ----------------------------
   // ------------------------------------------------------------------------------------
-  const [productId, setProductId] = useState('18083');
+  const [productId, setProductId] = useState('18080');
   const [selectedStyle, setSelectedStyle] = useState({});
   const [productInfo, setProductInfo] = useState({});
 

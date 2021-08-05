5d9db6ee5e2ffc9279709718f9784881847dc95b
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-05 15:02:59 -0600

    testing complete

--- tests/test.jsx
+++ tests/test.jsx
@@ -3,23 +3,33 @@
  */
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import axiosMock from 'axios';
 import App from '../client/Components/App';
-/**
- * @jest-environment jsdom
- */
+import testData from './testData';
+import testDataQA from './testDataQA';
 
-// describe('This is my first test', () => {
-//   test('and it does this', () => {
-//     expect(true).toBe(true);
-//   });
-//   test('and this second test does this', () => {
-//     expect(false).toBe(false);
-//   });
-// });
+describe('This is my first test', () => {
+  test('and it does this', () => {
+    expect(true).toBe(true);
+  });
+  test('and this second test does this', () => {
+    expect(false).toBe(false);
+  });
+});
+
+beforeEach(async () => {
+  axiosMock.get.mockResolvedValueOnce({ data: testData.productInfo });
+  axiosMock.get.mockResolvedValueOnce({ data: testData.productStyle });
+  axiosMock.get.mockResolvedValueOnce({ data: testDataQA });
+  axiosMock.get.mockResolvedValueOnce({ data: testData.reviews });
+  axiosMock.get.mockResolvedValueOnce({ data: testData.reviewMeta });
+});
+afterEach(cleanup);
 
 test('shows four reviews after clicking the button', async () => {
+  // axiosMock.get.mockResolvedValueOnce({data:});
   let component, button;
   await act(async () => {
     component = render(<App />);
@@ -28,16 +38,16 @@ test('shows four reviews after clicking the button', async () => {
     button = component.getByText('More Reviews');
     await fireEvent.click(button);
   });
-  // const reviews = component.getAllByTestId('reviewCard');
-  // expect(reviews).toHaveLength(4);
+  const reviews = component.getAllByTestId('reviewCard');
+  expect(reviews).toHaveLength(4);
 });
 
 test('loads the component and indicates as such, then displays Product Select', async () => {
   let component;
   await act(async () => {
     component = render(<App />);
-    expect(component.getByText('Loading...')).toBeInTheDocument();
+    expect(component.getAllByText('Loading...').length).toBeTruthy();
   });
-  // const [productSelect] = component.getAllByTestId('productSelect');
-  // expect(productSelect).toBeTruthy();
+  const [productSelect] = component.getAllByTestId('productSelect');
+  expect(productSelect).toBeTruthy();
 });

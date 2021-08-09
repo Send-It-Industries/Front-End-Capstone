e8ed5b1c90be3ac1fe77fb36b17ee9c9964daf11
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-09 12:56:04 -0600

    changed product number

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   // ------------------------------------------------------------------------------------
   // ------------------                State                 ----------------------------
   // ------------------------------------------------------------------------------------
-  const [productId, setProductId] = useState('18078');
+  const [productId, setProductId] = useState('18083');
   const [selectedStyle, setSelectedStyle] = useState({});
   const [productInfo, setProductInfo] = useState({});
 
